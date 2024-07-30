9f5c743bd981a1f57b69c6fd2eb931dbad524a30
wojciechp99 <wojciechp99@outlook.com>
2024-07-30 18:58:33 +0200

    initial commit

---
+++ src/app/components/chips/chips.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chips',
+  standalone: true,
+  imports: [],
+  templateUrl: './chips.component.html',
+  styleUrl: './chips.component.css'
+})
+export class ChipsComponent {
+
+}
